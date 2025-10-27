bfe27d1594d6461badae8be60e6dbd1ab5b8eb8e
nelvss <22-02551@g.batstate-u.edu.ph>
2025-10-27 12:09:37 +0800

    Deleting unnecessary files

--- my-express-web/hash-password.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-// Simple script to hash passwords for your Supabase users table
-const password = 'staff123'; // Change this to your desired password
-const hashedPassword = bcrypt.hashSync(password, 10);
-
-console.log('Password:', password);
-console.log('Hashed Password:', hashedPassword);
-console.log('\nCopy the hashed password above and paste it into your Supabase users table in the password_hash column.');
