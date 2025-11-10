c3f71033671d3b7bce01e7f99f0fcbef6b3eac37
nelvss <22-02551@g.batstate-u.edu.ph>
2025-11-11 03:03:02 +0800

    server

--- my-express-web/server.js
+++ my-express-web/server.js
@@ -22,7 +22,14 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Expires', 'X-Requested-With'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'Expires',
+    'X-Requested-With',
+    'Cache-Control',
+    'cache-control'
+  ],
   optionsSuccessStatus: 200
 }));
 
