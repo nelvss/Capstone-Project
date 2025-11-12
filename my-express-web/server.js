daf7c388eb2bd9b0c2ddfe16bac34afa7a865774
nelvss <22-02551@g.batstate-u.edu.ph>
2025-11-12 17:05:36 +0800

    Fix server route error - remove deprecated wildcard options route

--- my-express-web/server.js
+++ my-express-web/server.js
@@ -36,7 +36,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '10mb' })); // Increased limit for base64 image uploads
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
